299fb1a8edb74dfc1efb46ce80cef4ffe0b49e2b
Amit-Tiwari1 <aktsln28@gmail.com>
2025-03-28 10:49:46 +0530

    start week4

--- week3/script.js
+++ week3/script.js
@@ -1,10 +1,10 @@
-let count = 0
+// let count = 0
 
-function changeCount(){
-const getAllH2Tags = document.querySelectorAll("h2");
-getAllH2Tags[0].innerHTML= count;
-count = count + 1
-}
+// function changeCount(){
+// const getAllH2Tags = document.querySelectorAll("h2");
+// getAllH2Tags[0].innerHTML= count;
+// count = count + 1
+// }
 
-setInterval(changeCount,1000)
+// setInterval(changeCount,1000)
 
